98d3f25acaa7b25790e3ceceff1922991c335696
offbeat-dev <109253600+offbeat-dev@users.noreply.github.com>
2022-11-14 12:24:27 -0800

    fix splide object-fit

--- components/slider/Offbeat.vue
+++ components/slider/Offbeat.vue
@@ -8,3 +8,15 @@
     </SplideSlide>
   </Splide>
 </template>
+<style>
+.splide__list {
+  height: 50vh;
+}
+
+.splide__slide img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  object-position: center;
+}
+</style>
