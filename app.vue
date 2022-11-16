f506a75f104425d81074c40599a37e3cbeba6eca
offbeat-dev <offbeat.it.all@gmail.com>
2022-11-15 16:50:04 -0800

    :tada: -- add head with title

--- app.vue
+++ app.vue
@@ -1,11 +1,14 @@
 <template>
+  <Head>
+    <Title>offbeatDev : Paulo Guerra F. // portfolio</Title>
+  </Head>
   <HeaderOffbeat />
   <ProjectsOffbeat />
   <SliderOffbeat />
 </template>
 <style>
 html {
-  font-family: 'Dongle', sans-serif;
+  font-family: "Dongle", sans-serif;
   font-weight: 300;
   font-size: 24px;
 }
